6ed7da25c17d1de09730ca04ac0cb5723ebfc4a3
curly08 <mattynoakes08@gmail.com>
2021-10-07 15:32:00 -0400

    Refactor classes and add prepend method

--- main.rb
+++ main.rb
@@ -1,11 +1,20 @@
 class LinkedList
+  attr_reader :head, :tail, :next_node
   def initialize
-    @list = Array.new
+    @head = @next_node
+    @tail = nil
+    @next_node = @tail
   end
 
   # adds a new node containing value to the end of the list
   def append(value)
-    @list.push(Node.new(value))
+    @next_node = Node.new(value, @tail)
+    @head = @next_node
+  end
+
+  # adds a new node containing value to the start of the list
+  def prepend(value)
+    @next_node = Node.new(value, @next_node)
   end
 end
 
@@ -18,5 +27,7 @@ end
 
 list = LinkedList.new
 p list
-list.append(5)
+p list.append(5)
+p list
+p list.prepend(10)
 p list
