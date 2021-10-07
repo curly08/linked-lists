0aebcdf210107fecc270647ceeffd2f1c89cea31
curly08 <mattynoakes08@gmail.com>
2021-10-07 14:29:31 -0400

    Add append method to LinkedList

--- main.rb
+++ main.rb
@@ -1,13 +1,22 @@
 class LinkedList
+  def initialize
+    @list = Array.new
+  end
 
+  # adds a new node containing value to the end of the list
+  def append(value)
+    @list.push(Node.new(value))
+  end
 end
 
 class Node
-  def value
-    nil
-  end
-
-  def next_node
-    nil
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
   end
 end
+
+list = LinkedList.new
+p list
+list.append(5)
+p list
